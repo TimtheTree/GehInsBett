682cfd3f68cfc555210dde37352dd5f393f79d7c
TimtheTree <tim@verbout.de>
2022-11-05 16:22:39 +0100

    checks the time every minute and shuts the pc down at 22:00

---
+++ BettTimer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace GehInsBett;
+
+public static class BettTimer
+{
+    public static void CheckTime()
+    {
+        var timer = new Timer(TimeSpan.FromMinutes(1));
+        timer.Start();
+        timer.Elapsed += TimerOnElapsed;
+    }
+
+    private static void TimerOnElapsed(object? sender, ElapsedEventArgs e)
+    {
+        if (DateTime.Now.Hour == 22 && DateTime.Now.Minute == 0)
+        {
+            Process.Start("shutdown.exe", "/s /t 0 /c 'Geh ins Bett!'");
+        }
+    }
+}
